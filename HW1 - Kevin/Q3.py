d5d208a540fa4589f45042b4311e28fc1eb48ba1
Kevin Yang <40809478+keviny2@users.noreply.github.com>
2021-09-20 10:02:35 -0700

    finished Q3

--- HW1 - Kevin/Q3.py
+++ HW1 - Kevin/Q3.py
@@ -43,21 +43,16 @@ for c in range(2):
             for w in range(2):
                 p[c, s, r, w] = p_C(c) * p_S_given_C(s, c) * p_R_given_C(r, c) * p_W_given_S_R(w, s, r)
 
-joint = 0
-for s in range(2):
-    for r in range(2):
-        joint += p[1, s, r, 1]
 
-normalizing_const = 0
+p_C_given_W = np.zeros(2)
 for c in range(2):
     for s in range(2):
         for r in range(2):
-            normalizing_const += p[c, s, r, 1]
+            p_C_given_W[c] += p[c, s, r, 1]
 
-p_C_given_W = joint / normalizing_const
+p_C_given_W /= np.sum(p_C_given_W)
 
-# print('There is a {:.2f}% chance it is cloudy given the grass is wet'.format(p_C_given_W[1] * 100))
-print('There is a {:.2f}% chance it is cloudy given the grass is wet'.format(p_C_given_W * 100))
+print('There is a {:.2f}% chance it is cloudy given the grass is wet'.format(p_C_given_W[1] * 100))
 
 ##2. ancestral sampling and rejection:
 # https://www.cs.ubc.ca/~fwood/CS532W-539W/lectures/mcmc.pdf
@@ -111,6 +106,16 @@ samples = np.zeros(num_samples)
 state = np.zeros(4, dtype='int')
 # c,s,r,w, set w = True
 
-# TODO
+c, s, r, w = 0, 1, 2, 3
+i = 0
+state[w] = 1
+while i < num_samples:
+    state[c] = np.argmax(np.random.multinomial(1, p_C_given_S_R[:, state[s], state[r]]))
+    state[s] = np.argmax(np.random.multinomial(1, p_S_given_C_R_W[state[c], :, state[r], state[w]]))
+    state[r] = np.argmax(np.random.multinomial(1, p_R_given_C_S_W[state[c], state[s], :, state[w]]))
+
+    samples[i] = state[c]
+    i += 1
+
 
 print('The chance of it being cloudy given the grass is wet is {:.2f}%'.format(samples.mean() * 100))
