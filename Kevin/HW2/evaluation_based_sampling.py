80f3aa6805243630af4a45dfc692e2182ffdd50e
Kevin <kevinyang10@gmail.com>
2021-10-16 15:59:09 -0700

    working on bug with let statement

--- Kevin/HW2/evaluation_based_sampling.py
+++ Kevin/HW2/evaluation_based_sampling.py
@@ -1,7 +1,9 @@
 from daphne import daphne
 from tests import is_tol, run_prob_test,load_truth
 import torch
-import numpy as np
+from torch.distributions import normal, beta, exponential
+
+variable_bindings = {}
 
 def evaluate_program(ast):
     """Evaluate a program as desugared by daphne, generate a sample from the prior
@@ -13,10 +15,47 @@ def evaluate_program(ast):
     if type(ast) is not list:
         return ast
 
+    # BUG: THERE IS A BUG IN MY CODE FOR LET!!!!!
+    # substitute bound variables for their values
+
+    # construct boolean array indicating which elements are variables that are bound to a value using the global
+    # variable_bindings dictionary
+    variables = list(variable_bindings.keys())
+    variable_bindings_bool = [elem in variables for elem in ast]
+
+    # substitute the variable name in the ast with the actual value
+    if any(variable_bindings_bool):
+        for idx, val in enumerate(variable_bindings_bool):
+            if val:
+                ast[idx] = variable_bindings[ast[idx]]
+
+
+    if ast[0] == 'let':
+        # we will successfully assign a variable a value after this block of code executes
+        # BUG: weird behavior
+        binding_obj = evaluate_program(ast[1])
+        variable_bindings[binding_obj[0]] = binding_obj[1]
+
+        # throw away let statement and continue evaluation
+        return evaluate_program(ast[2:])
+
+    if ast[0] == 'sample':
+        distribution_obj = evaluate_program(ast[1])
+        if distribution_obj[0] == 'normal':
+            return normal.Normal(distribution_obj[1], distribution_obj[2]).sample()
+        if distribution_obj[0] == 'beta':
+            return beta.Beta(distribution_obj[1], distribution_obj[2]).sample()
+        if distribution_obj[0] == 'exponential':
+            return exponential.Exponential(distribution_obj[1]).sample()
+
     # [+ 3 4]
     if all([type(elem) is not list for elem in ast]):
         if type(ast[0]) in [int, float, torch.Tensor]:
+            return torch.tensor(ast[0])
+        if type(ast[0]) is dict:
             return ast[0]
+        if ast[0] in ['normal', 'beta', 'exponential']:
+            return ast
         elif ast[0] == '+':
             return torch.sum(torch.tensor(ast[1:]))
         elif ast[0] == 'sqrt':
@@ -30,26 +69,31 @@ def evaluate_program(ast):
         elif ast[0] == 'vector':
             return torch.tensor(ast[1:])
         elif ast[0] == 'get':
-            return ast[1][ast[2]]
+            if type(ast[1]) is dict:
+                return torch.tensor(ast[1][ast[2]])
+            return torch.tensor(ast[1][ast[2]])
         elif ast[0] == 'put':
-            ast[1][ast[2]] = ast[3]
+            if type(ast[1]) is dict:
+                ast[1][ast[2]] = torch.tensor(ast[3])
+            else:
+                ast[1][ast[2]] = torch.tensor(ast[3])
             return ast[1]
         elif ast[0] == 'first':
-            return ast[1][0]
+            return torch.tensor(ast[1][0])
         elif ast[0] == 'last':
-            return ast[1][-1]
+            return torch.tensor(ast[1][-1])
         elif ast[0] == 'append':
             res = ast[1].tolist()
             res.extend([ast[2]])
             return torch.tensor(res)
         elif ast[0] == 'hash-map':
-            pass
+            return dict(zip(ast[1:][::2], torch.tensor(ast[1:][1::2])))
         else:
             return ast
 
     # [+ [- 3 1] [* 2 4]]
     subroot = [evaluate_program(sub_ast) for sub_ast in ast]
-    return torch.tensor(evaluate_program(subroot))
+    return evaluate_program(subroot)
 
 
 def get_stream(ast):
@@ -61,7 +105,7 @@ def get_stream(ast):
 
 def run_deterministic_tests():
 
-    debug_start = 11
+    debug_start = 1
     for i in range(debug_start,14):
         #note: this path should be with respect to the daphne path!
         ast = daphne(['desugar', '-i', '../CPSC532W-HW/Kevin/HW2/programs/tests/deterministic/test_{}.daphne'.format(i)])
@@ -82,10 +126,11 @@ def run_probabilistic_tests():
     
     num_samples=1e4
     max_p_value = 1e-4
-    
-    for i in range(1,7):
+
+    debug_start = 4
+    for i in range(debug_start,7):
         #note: this path should be with respect to the daphne path!        
-        ast = daphne(['desugar', '-i', '../CS532-HW2/programs/tests/probabilistic/test_{}.daphne'.format(i)])
+        ast = daphne(['desugar', '-i', '../CPSC532W-HW/Kevin/HW2/programs/tests/probabilistic/test_{}.daphne'.format(i)])
         truth = load_truth('programs/tests/probabilistic/test_{}.truth'.format(i))
         
         stream = get_stream(ast)
@@ -94,13 +139,14 @@ def run_probabilistic_tests():
         
         print('p value', p_val)
         assert(p_val > max_p_value)
+        print('Test passed', ast, 'test', i)
     
     print('All probabilistic tests passed')    
 
         
 if __name__ == '__main__':
 
-    run_deterministic_tests()
+    # run_deterministic_tests()
     
     run_probabilistic_tests()
 
