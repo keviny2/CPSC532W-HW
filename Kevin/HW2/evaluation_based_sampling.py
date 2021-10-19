647ee7d9e13041d52ef679484944bb674ed4c53c
Kevin <kevinyang10@gmail.com>
2021-10-19 07:04:42 -0700

    i am screwed

--- Kevin/HW2/evaluation_based_sampling.py
+++ Kevin/HW2/evaluation_based_sampling.py
@@ -1,4 +1,5 @@
 import copy
+import pickle
 
 from daphne import daphne
 from tests import is_tol, run_prob_test,load_truth
@@ -215,7 +216,8 @@ def run_deterministic_tests():
     debug_start = 1
     for i in range(debug_start,14):
         #note: this path should be with respect to the daphne path!
-        ast = daphne(['desugar', '-i', '../CPSC532W-HW/Kevin/HW2/programs/tests/deterministic/test_{}.daphne'.format(i)])
+        # ast = daphne(['desugar', '-i', '../CPSC532W-HW/Kevin/HW2/programs/tests/deterministic/test_{}.daphne'.format(i)])
+        ast = load_ast('programs/saved_asts/det{}_ast.pkl'.format(i))
         truth = load_truth('programs/tests/deterministic/test_{}.truth'.format(i))
         ret, sig = evaluate_program(ast), '0'
         try:
@@ -234,47 +236,49 @@ def run_probabilistic_tests():
     num_samples=1e4
     max_p_value = 1e-4
 
-    debug_start = 6
+    debug_start = 1
     for i in range(debug_start,7):
         #note: this path should be with respect to the daphne path!        
-        ast = daphne(['desugar', '-i', '../CPSC532W-HW/Kevin/HW2/programs/tests/probabilistic/test_{}.daphne'.format(i)])
+        # ast = daphne(['desugar', '-i', '../CPSC532W-HW/Kevin/HW2/programs/tests/probabilistic/test_{}.daphne'.format(i)])
+        ast = load_ast('programs/saved_asts/prob{}_ast.pkl'.format(i))
         truth = load_truth('programs/tests/probabilistic/test_{}.truth'.format(i))
-        
+
         stream = get_stream(ast)
-        
+
         p_val = run_prob_test(stream, truth, num_samples)
-        
+
         print('p value', p_val)
         assert(p_val > max_p_value)
         print('Test passed', ast, 'test', i)
     
-    print('All probabilistic tests passed')    
+    print('All probabilistic tests passed')
 
-        
+def save_ast(file_name, my_ast):
+    # faster load
+
+    # saving the list
+    open_file = open(file_name, "wb")
+    pickle.dump(my_ast, open_file)
+    open_file.close()
+
+def load_ast(file_name):
+
+    # loading the list
+    open_file = open(file_name, 'rb')
+    ret = pickle.load(open_file)
+    open_file.close()
+    return ret
+
 if __name__ == '__main__':
 
-    # run_deterministic_tests()
+    run_deterministic_tests()
     
-    # run_probabilistic_tests()
+    run_probabilistic_tests()
 
-    debug_start = 3
+    debug_start = 1
     for i in range(debug_start,5):
         # ast = daphne(['desugar', '-i', '../CPSC532W-HW/Kevin/HW2/programs/{}.daphne'.format(i)])
 
-        # faster load
-        import pickle
-
-        # saving the list
-        # file_name = "daphne3_ast.pkl"
-        #
-        # open_file = open(file_name, "wb")
-        # pickle.dump(ast, open_file)
-        # open_file.close()
-
-        # loading the list
-        open_file = open('daphne3_ast.pkl', 'rb')
-        ast = pickle.load(open_file)
-        open_file.close()
-
+        ast = load_ast('programs/saved_asts/daphne{}_ast.pkl'.format(i))
         print('\n\n\nSample of prior of program {}:'.format(i))
         print(evaluate_program(ast)[0])
--- Kevin/HW2/primitives.py
+++ Kevin/HW2/primitives.py
@@ -1,3 +1,52 @@
 import torch
 
+simple_operations = ['+', '-', '*', '/', 'sqrt', 'vector', 'hash-map', 'get', 'put', 'first',
+                     'second', 'rest', 'last', 'append', '<', '<=', '>', '>=', '==', 'mat-transpose',
+                     'mat-tanh', 'mat-mul', 'mat-add', 'mat-repmat']
+
+complex_operations = ['sample', 'let', 'if', 'defn', 'observe']
+
+
+def evaluate_simple_operation(ast):
+    if ast[0] == '+':
+        return torch.sum(torch.tensor(ast[1:]))
+    elif ast[0] == '-':
+        return ast[1] - torch.sum(torch.tensor(ast[2:]))
+    elif ast[0] == '*':
+        return torch.prod(torch.tensor(ast[1:]))
+    elif ast[0] == '/':
+        return ast[1] / torch.prod(torch.tensor(ast[2:]))
+    elif ast[0] == 'sqrt':
+        return torch.sqrt(torch.tensor(ast[1]))
+    elif ast[0] == '<':
+        return ast[1] < ast[2]
+    elif ast[0] == '>':
+        return ast[1] > ast[2]
+    elif ast[0] == 'get':
+        if type(ast[1]) is dict:
+            return torch.tensor(ast[1][ast[2]])
+        return torch.tensor(ast[1][ast[2]])
+    elif ast[0] == 'put':
+        if type(ast[1]) is dict:
+            ast[1][ast[2]] = torch.tensor(ast[3])
+        else:
+            ast[1][ast[2]] = torch.tensor(ast[3])
+        return ast[1]
+    elif ast[0] == 'remove':
+        del ast[1][ast[2]]
+        return ast[1]
+    elif ast[0] == 'first':
+        return torch.tensor(ast[1][0])
+    elif ast[0] == 'second':
+        return torch.tensor(ast[1][1])
+    elif ast[0] == 'last':
+        return torch.tensor(ast[1][-1])
+    elif ast[0] == 'rest':
+        return torch.tensor(ast[1][1:])
+    elif ast[0] == 'append':
+        res = ast[1].tolist()
+        res.extend([ast[2]])
+        return torch.tensor(res)
+    elif ast[0] == 'hash-map':
+        return dict(zip(ast[1:][::2], torch.tensor(ast[1:][1::2])))
 #TODO
